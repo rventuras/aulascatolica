b7a0ebef3ccd1f8a2fd1d5afdd0adcddc92e656a
rventuras <111456154+rventuras@users.noreply.github.com>
2022-09-08 13:55:44 -0300

    Salario por horas trabalhadas

---
+++ salario.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	char[100] nomeFuncionario, numeroMatricula;
+	char sexo;
+	int nHorasTrabalhadas, valorPorHora, salario
+	printf("Nome do funcionario? ");
+	scanf("%s", nomeFuncionario);
+	printf("Numero da matricula? ");
+	scanf("%s", numeroMatricula);
+	printf("Sexo?");
+	scanf("%c", sexo)
+	printf("Numero de horas trabalhadas?");
+	scanf("%d", nHorasTrabalhadas);
+	printf("Valor de horas trabalhadas?");
+	scanf("%d", valorPorHora);
+	salario = valorPorHora * nHorasTrabalhadas;
+	
+	
+	
+	
+
+}
