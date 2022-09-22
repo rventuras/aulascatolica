b6ee151235cbf39ecf4ff929fd01cbba5004f0e2
rventuras <111456154+rventuras@users.noreply.github.com>
2022-09-22 13:18:39 -0300

    Add files via upload

---
+++ nummaioremenor.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+	int num1, num2;
+	printf("Insira numero 1 e numero 2: ");
+	scanf("%d%d",&num1, &num2);
+	if (num1 > num2)
+	printf("%d e maior", num1);
+	else
+	printf("%d e maior", num2);
+
+
+
+}
